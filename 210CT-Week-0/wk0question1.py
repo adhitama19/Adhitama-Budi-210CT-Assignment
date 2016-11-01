aac7c777b06f36a2851642184d0c79f4f4ebaa55
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-01 14:21:14 +0000

    Update and rename 210CT-Week-1/Question 1 to 210CT-Week-0/wk0question1.py

---
+++ 210CT-Week-0/wk0question1.py
@@ -0,0 +1,18 @@
+"""Question 1"""
+
+varA = 30
+varB = 6
+varC = 20
+varD = 10
+
+fracA = varA//varB
+print ( fracA, type(fracA))
+
+fracB = varC//varD
+print ( fracB, type(fracB))
+
+if fracA > fracB:
+    print (fracA)
+
+else:
+    print ('None')
