2869543019e191333730e802daa67bf82b19ea30
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-02 17:03:31 +0000

    Create wk0question5.py

---
+++ 210CT-Week-0/wk0question5.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+now = date.now()
+
+
+
+def countDays (days, month, year):
+	date_format = "%d/%m/%y"
+	now_date = datetime.now(date_format)
+	input_date = datetime.strptime()
+	
