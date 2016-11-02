a98e8410580bfa8b8b2737d73121e906718c3028
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-02 17:25:56 +0000

    Update wk0question5.py

--- 210CT-Week-0/wk0question5.py
+++ 210CT-Week-0/wk0question5.py
@@ -1,11 +1,18 @@
 from datetime import date
 
-now = date.now()
-
-
 
 def countDays (days, month, year):
-	date_format = "%d/%m/%y"
-	now_date = datetime.now(date_format)
-	input_date = datetime.strptime()
+	input_Date = date(year, month, days)
+	final_Date = date(2016, 12, 31)
+	count_Days = final_Date - input_Date
+	count_days_count = int(count_Days.days)
+	days_Passed = int(365 - count_days_count)
+	return ("days passed: %d , days remaining: %d" % (count_days_count, days_Passed))
+	
+
+d = 26
+m = 9
+y = 2016
+
+print(countDays(d, m, y))
 	
