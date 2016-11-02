92ec0b390afe60dd61c6a341c53d1c71fa6f4ed8
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-02 17:42:17 +0000

    Create wk0question6.py

---
+++ 210CT-Week-0/wk0question6.py
@@ -0,0 +1,8 @@
+def minMaxFind (list_One):
+	max_Val = max(list_One)
+	min_Val = min(list_One)
+	
+	max_position = list_One.index(max_Val) 
+	min_position = list_One.index(min_Val)
+	
+	return(max_Val, )
