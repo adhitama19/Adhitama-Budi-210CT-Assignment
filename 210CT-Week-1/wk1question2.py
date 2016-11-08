aea6987124b6c1b78f1c35e1a313b987484fff91
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-08 09:56:47 +0000

    Update wk1question2.py

--- 210CT-Week-1/wk1question2.py
+++ 210CT-Week-1/wk1question2.py
@@ -1,1 +1,14 @@
+def count_factorial(n):
 
+    factorial_list = []
+    result = 1
+
+    for i in range (1, n+1):
+        factorial_list.append(i)
+
+    for x in factorial_list:
+        result *= x
+
+    return result
+        
+print(count_factorial(10))
