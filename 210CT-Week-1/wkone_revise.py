6c87cb25572dea3ae8a46acae5535d7b10dd6c39
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-08 10:21:55 +0000

    Create wkone_revise.py

---
+++ 210CT-Week-1/wkone_revise.py
@@ -0,0 +1,20 @@
+
+import random
+from random import randrange
+
+def shuffle_number(numbers):
+    empty_lst = []
+
+    for i in numbers:
+        empty_lst.append(i)
+
+    for x in empty_lst:
+        random_index = randrange(0, )
+        random_object = random.choice(empty_lst)
+        empty_lst.insert(random_index, empty_lst.pop(random_index))
+
+    return empty_lst
+
+n = [1, 2, 3, 4, 5, 6]
+
+print(shuffle_number(n))
