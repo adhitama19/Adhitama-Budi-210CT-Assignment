35140eb4316dbde47eb435fb67507f3e16290d12
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-08 10:29:53 +0000

    Update wk1question1.py

--- 210CT-Week-1/wk1question1.py
+++ 210CT-Week-1/wk1question1.py
@@ -1,4 +1,6 @@
-from random import *
+import random
+from random import randrange
+
 
 def shuffle_number(numbers):
     empty_lst = []
@@ -6,15 +8,13 @@ def shuffle_number(numbers):
     for i in numbers:
         empty_lst.append(i)
 
-    empty_lst.insert(4, empty_lst.pop(3))
-    empty_lst.insert(3, empty_lst.pop(2))
-    empty_lst.insert(0, empty_lst.pop(4))
-
-
+    for x in empty_lst:
+        random_index = randrange(0, len(empty_lst))
+        random_object = random.choice(empty_lst)
+        empty_lst.insert(random_index, empty_lst.pop(x))
 
     return empty_lst
 
+n = [1, 2, 3, 4, 5, 6]
 
-iseng = [1, 2, 3, 4, 5]
-
-print(shuffle_number(iseng))
+print(shuffle_number(n))
