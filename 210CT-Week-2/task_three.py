9f22f5a3563d34b3dafadf21ff0417f12d381411
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-08 14:29:22 +0000

    Update task_three.py

--- 210CT-Week-2/task_three.py
+++ 210CT-Week-2/task_three.py
@@ -1,1 +1,29 @@
+def matrix_Addition (b, c):
 
+
+    result = [[0,0],
+              [0,0],
+              [0,0]]
+
+    
+
+    for row in range(len(b)):
+        for column in range (len(c)):
+            result [row][column] = b[row][column] + c[row][column]
+
+    for r in result:
+        print (result)
+
+    return result
+
+
+matrix_b = [[12, 14],
+            [15, 2],
+            [3, 7]]
+
+matrix_c = [[2, 3],
+            [5, 13],
+            [2, 7]]
+
+
+print(matrix_Addition(matrix_b, matrix_c))
