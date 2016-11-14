9915ebe83c4c6dc901f5fcdbb3b7ed6ea30ea33a
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-14 17:48:47 +0000

    Update wk1question2.py

--- 210CT-Week-1/wk1question2.py
+++ 210CT-Week-1/wk1question2.py
@@ -1,6 +1,7 @@
 def count_factorial(n):
 
     factorial_list = []
+    trail_zeros = 0
     result = 1
 
     for i in range (1, n+1):
@@ -9,6 +10,20 @@ def count_factorial(n):
     for x in factorial_list:
         result *= x
 
-    return result
+    for y in factorial_list:
+        while y > 0:
+            if y % 5 == 0:
+                trail_zeros = trail_zeros + 1
+                y = y/5
+            else:
+                break
+                
+
+    return result, trail_zeros
+
+
+    
+
+    
         
-print(count_factorial(10))
+print(count_factorial(5))
