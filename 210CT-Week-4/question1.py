ced47a1686bcfab799b9605defbc8cdb4907cf80
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-22 14:07:34 +0000

    Update question1.py

--- 210CT-Week-4/question1.py
+++ 210CT-Week-4/question1.py
@@ -1,26 +1,30 @@
-def binarySearch(alist, low, high, item):         #This will create Binary Search function with its parameters.
-    first = 0                                     #This is the first value of the list
-    last = len(alist)-1                           #The last value for a list
-    found = False                                 #Found is set as false so while loop can then run
+def binarySearch(alist, low, high):         
+    first = 0                                    
+    last = len(alist)-1                           
+    found = False                                 
 
-    while first <= last and not found:            #While loop which indicates while first number is less tahn or equal to last and true.
-        midpoint = (first + last)//2              #set the mid point
-        lowVal = low < item                       #This will be the low set interval
-        highVal = high > item                     # This will be the high set interval
-        if alist[midpoint] == item:               #statement if midpoint is equal with the item return True
-            found = True
-        else:
-            if item < alist[midpoint]:
-                last = midpoint - 1
+    while first <= last and not found:           
+        midpoint = (first + last)//2
+
+        if low in alist and high in alist:
+            
+            if alist[midpoint] > low and alist[midpoint] < high:
+                found = True
+            
             else:
-                first = midpoint+1
+                if alist[midpoint] > low :
+                    last = midpoint - 1
+                else:
+                    first = midpoint + 1
 
+        else:
+            return found
+        
     return found
 
-list_One = [2, 3, 4, 5, 6, 10, 11, 12]
-low = 9
-high = 11
-item = 10
+list_One = [2, 3, 4, 5, 6, 10, 11, 12, 14]
+low = 5
+high = 10
 
-print(binarySearch(list_One, low, high, item))
+print(binarySearch(list_One, low, high))
 
