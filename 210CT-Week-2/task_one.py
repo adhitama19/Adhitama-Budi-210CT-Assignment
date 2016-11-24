df35dc40c1356a45ec2f4419e84849045cee95d0
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-24 21:57:40 +0000

    Update task_one.py

--- 210CT-Week-2/task_one.py
+++ 210CT-Week-2/task_one.py
@@ -1,23 +1,26 @@
-from math import sqrt
+""" This is the question 3 coursework solution. It asks us to returns the highest perfect square of a value.
+    If the value does not have the perfect square, it should return the lower highest perfect square of that
+    value. """
 
-def fact_number(number):
+from math import sqrt                     # import built-in sqrt function from math module
 
-    perfect_square = []
-    yang_disamain = []
-    n_int = int(number)
+def fact_number(number):                  # a new function called fact_number is defined with number as the parameter
 
-
-    for i in range (1, number-1):
-            new_val = int(i * i)
-            perfect_square.append(new_val)
-
-    if number in perfect_square:
-        return sqrt(number)
-
-    else:
-        final_val = perfect_square[-1]
-        final_Ans = sqrt(final_val)
-        return int(sqrt(final_Ans))
+    perfect_square = []                   # empty list called perfect_square that will later be appended
 
 
-print (fact_number(20))
+    for i in range (1, number-1):           # a for loop that will go through range 1 to number parameter with less 1 value
+            new_val = (i * i)               # a variable to multiply the square root of i
+            perfect_square.append(new_val)  # perfect_square empty list will be appended with new_val value which creates a list with number that have a square root
+
+    if number in perfect_square:            # conditional statement to check if the input number in perfect_square list
+        return sqrt(number)                 # statements that will return the square root of the input number
+
+    else:                                   # statements that will be executed if input number is not perfect square
+        final_val = perfect_square[-1]      # this will get the last value in a list that is lower 1 value of the input
+        final_Ans = sqrt(final_val)         # this variable will be the square root of final_val
+        
+        return "The perfect square of the number less than the input is: %d" % int(sqrt(final_Ans))      #it will then return the sqrt of final_Ans as the answer
+
+
+print (fact_number(35))
