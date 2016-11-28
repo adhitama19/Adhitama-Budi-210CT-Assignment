ce4d1df79192fe9b17c1f39c74003d2226f4a600
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-28 20:24:49 +0000

    Update task_one.py

--- 210CT-Week-5/task_one.py
+++ 210CT-Week-5/task_one.py
@@ -1,23 +1,29 @@
-def asc_Sequence (n):
-    list_one = []
+list_one = [1, 2, 3, 4, 5, 1, 2, 3, 2, 3, 5]
 
-    for i in n:
-        list_one.append(i)
 
-    for x in n:
-        if x == max(list_one):
-            index_max = list_one.index(max(list_one))
-            list_one[0] = list_one[index_max]
+def split_substring(split_list):
+
+    first_elm = split_list[0]
+    sub_list = [[first_elm]]
+
+    for elm in split_list[1:]:
+        if elm > first_elm:
+            first_elm = elm
+            sub_list[-1].append(elm)
             
-        
 
-    return list_one
+        else:
+            first_elm = elm
+            sub_list.append([first_elm])
+
+    maximum_sublist = max(sub_list, key = len)
+    count_sublist = len(sub_list)
 
     
-
+    print ("There are %d sub-list found from this list" % count_sublist)
+    print ("The maximum substring in this set of list is: %s" % maximum_sublist)
+    return sub_list
     
 
 
-list_n = [12, 45, 33, 53]
-
-print(asc_Sequence(list_n))
+print(split_substring(list_one))
