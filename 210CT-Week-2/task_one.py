6296240273e15b3672871852ff7a4459887e3b74
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-29 22:04:14 +0000

    Update task_one.py

--- 210CT-Week-2/task_one.py
+++ 210CT-Week-2/task_one.py
@@ -9,18 +9,18 @@ def fact_number(number):                  # a new function called fact_number is
     perfect_square = []                   # empty list called perfect_square that will later be appended
 
 
-    for i in range (1, number-1):           # a for loop that will go through range 1 to number parameter with less 1 value
+    for i in range (1, number):           # a for loop that will go through range 1 to number parameter with less 1 value
             new_val = (i * i)               # a variable to multiply the square root of i
             perfect_square.append(new_val)  # perfect_square empty list will be appended with new_val value which creates a list with number that have a square root
 
     if number in perfect_square:            # conditional statement to check if the input number in perfect_square list
-        return sqrt(number)                 # statements that will return the square root of the input number
+        print ("The square root for your value is: %d" % int(sqrt(number)))                 # statements that will return the square root of the input number
 
     else:                                   # statements that will be executed if input number is not perfect square
         final_val = perfect_square[-1]      # this will get the last value in a list that is lower 1 value of the input
-        final_Ans = sqrt(final_val)         # this variable will be the square root of final_val
+        final_Ans = int(sqrt(sqrt(final_val)))         # this variable will be the square root of final_val
         
-        return "The perfect square of the number less than the input is: %d" % int(sqrt(final_Ans))      #it will then return the sqrt of final_Ans as the answer
+        print ("The perfect square of the number less than the input is: %d" % final_Ans)      #it will then return the sqrt of final_Ans as the answer
+    
 
-
-print (fact_number(35))
+fact_number(1025)
