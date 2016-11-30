9724754bf2b63e243e816aff44c9448e39fab558
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-11-30 22:18:28 +0000

    Update question_two.py

--- 210CT-Week-3/question_two.py
+++ 210CT-Week-3/question_two.py
@@ -1,23 +1,44 @@
 """This is the solution for question 7 coursework. The question ask us to create a recursive function
    to find if a number is a prime number or not. the function will be called as recursive_prime with
    'a' and 'number' as the input. number will be the value that is going to be checked if its prime
-   or not. while a will be the value that will keep on increasing until it is not bigger than nummber"""
+   or not. while a will be the value that will keep on increasing until it is not bigger than number"""
 
-def recursive_prime (div, number): # new function is defined as recursive_prime. with parameter a and number
+def recursive_prime (div, number):
 
-    if number < 1:               # conditional statement to check if number is less than 1. since number less than 1 are definitely not prime numbers
-        return False             # this will return False
+    if number < 1:
+        return False
 
-    else:                        # another conditional statement
-        while div < number:        # a while loop that will be executed if div is lower than number
+    else:
+        if div < number:      #base case
 
-            if (number % div) == 0: 
-                return False        
+            if (number % div) == 0:
+                return False
 
             else:
-                return recursive_prime (div + 1, number) 
+                return recursive_prime (div + 1, number)
 
-        return True # if there are no remainders it is returned as true.
+        return True
 
 
-print (recursive_prime(2, 13))
+print (recursive_prime(2, 30))
+print (recursive_prime(2, 41))
+
+
+"""
+PSEUDOCODE:
+
+RECURSIVE_PRIME (DIV, NUMBER)
+
+    IF number < 1:
+        RETURN FALSE
+
+    ELSE:
+        IF div < number:        //Base Case
+
+            IF number divided by div has no remainder
+                RETURN FALSE
+
+            ELSE
+                RETURN RECURSIVE_PRIME (DIV + 1, NUMBER)
+
+"""
