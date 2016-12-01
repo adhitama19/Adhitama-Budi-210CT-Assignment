5e170f30df7a7130383ed4fe5a2190790b791bf2
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-12-01 22:08:46 +0000

    Update task_one.py

--- 210CT-Week-2/task_one.py
+++ 210CT-Week-2/task_one.py
@@ -1,26 +1,71 @@
 """ This is the question 3 coursework solution. It asks us to returns the highest perfect square of a value.
     If the value does not have the perfect square, it should return the lower highest perfect square of that
-    value. """
+    value.
 
-from math import sqrt                     # import built-in sqrt function from math module
+    Input: 16
+    Output: 4
 
-def fact_number(number):                  # a new function called fact_number is defined with number as the parameter
+    Input: 20
+    Output: 4
 
-    perfect_square = []                   # empty list called perfect_square that will later be appended
+
+"""
 
 
-    for i in range (1, number):           # a for loop that will go through range 1 to number parameter with less 1 value
-            new_val = (i * i)               # a variable to multiply the square root of i
-            perfect_square.append(new_val)  # perfect_square empty list will be appended with new_val value which creates a list with number that have a square root
 
-    if number in perfect_square:            # conditional statement to check if the input number in perfect_square list
-        print ("The square root for your value is: %d" % int(sqrt(number)))                 # statements that will return the square root of the input number
+def fact_number(number):
 
-    else:                                   # statements that will be executed if input number is not perfect square
-        final_val = perfect_square[-1]      # this will get the last value in a list that is lower 1 value of the input
-        final_Ans = int(sqrt(sqrt(final_val)))         # this variable will be the square root of final_val
-        
-        print ("The perfect square of the number less than the input is: %d" % final_Ans)      #it will then return the sqrt of final_Ans as the answer
-    
+    perfect_square = []
 
-fact_number(1025)
+
+    for i in range (1, number):
+            new_val = (i * i)                               
+            perfect_square.append(new_val)                                  # numbers that have perfect square appended to perfect_square list
+
+    if number in perfect_square:
+        sqrt = int(number ** (0.5))                                         # to get the perfect square of the number
+        statereturn = ("The square root for your value is: %d" % (sqrt))
+        return (statereturn)
+
+    else:
+        final_Val = perfect_square[-1]                                      # get the last element from perfect square list
+        final_Sqrt = int((final_Val ** 0.5) ** 0.5)                         # to get the square root that is less than input number                         
+        final_ans = final_Sqrt
+
+        statereturn = ("The perfect square of the number less than the input is: %d" % (final_ans))         # statement that is returned when the input value is not perfect square
+        return (statereturn)
+
+    return
+
+
+
+
+print(fact_number(20))
+
+
+"""
+
+PSEUDOCODE:
+
+
+FACT_NUMBER (NUMBER)
+
+    perfect_square <- []
+
+    FOR i <- 1 to number
+        new_val <- (i * i)
+        new_val appended to perfect_square empty list
+
+    IF number is inside perfect_square
+        do square root calculation
+        return square root of the number //output
+
+    ELSE number is not in perfect_square
+        final_Val <- perfect_square[-1] //get final value from list
+        do square root calculation
+        final_ans <- final_Sqrt
+        return the square root that is less from number //output
+
+
+
+"""
