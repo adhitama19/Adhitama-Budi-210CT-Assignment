59ebae978b93254bc263a51854b703e07ceec489
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-12-01 19:28:02 +0000

    Update wk1question1.py

--- 210CT-Week-1/wk1question1.py
+++ 210CT-Week-1/wk1question1.py
@@ -1,22 +1,27 @@
 """ This is the week 1 question 1 solution. The question ask us to write a function that shuffles an array
     of integers and should explain the rationale behind its implementation. For this question the function
-    will be named shuffle_number and the parameter will be named numbers. The code will be commented to
-    tell what each line do. """
+    will be named shuffle_number and the parameter will be named numbers. 
+    
+    The output will return a random shuffled elements
+    Input: [1, 2, 3, 4, 5]
+    Potential output: [1, 4, 2, 3, 5] 
+
+"""
  
 
-from random import randrange  # This will import randrange function, which will shuffles number within range specified
+from random import randrange                                # Import random range
 
 
-def shuffle_number(list_number): # A new function is defined with shuffle_number as name, and numbers as the parameter
+def shuffle_number(list_number): 
     
     new_list = []
     
-    for x in list_number:   # for loop is made which will go through all value in empty_lst.
+    for x in list_number:                                   # for loop to go through each element in list_number.
         
-        random_index = randrange(0, len(list_number)-1) # using the built-in function randrange. This will give random_index a random value to shuffle numbers from its index
-        new_list.insert(random_index, x) # This is the important line to shuffle the numbers. the object (x) will be inserted in a random index.
+        random_index = randrange(0, len(list_number)-1)     # using randrange to give random index 
+        new_list.insert(random_index, x)                    # value x will be inserted to new_list with random index specified
 
-    return new_list #returns the empty_lst with new shuffled arrays
+    return new_list 
 
 n = [5, 4, 2, 3, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 
