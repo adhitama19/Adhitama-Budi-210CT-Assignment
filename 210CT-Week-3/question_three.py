d34ea5e3c87cb397bba0e13362b13c4edd8b45ec
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-12-02 19:07:54 +0000

    Update question_three.py

--- 210CT-Week-3/question_three.py
+++ 210CT-Week-3/question_three.py
@@ -2,19 +2,40 @@
     used for this solution is by using recursive function. The function will be defined as recursive_vowel with
     string as its parameter. further explaination will be explained through comments on the code """
 
-vowels = ["A", "I", "U", "E", "O", "a", "i", "u", "e", "o"] # List of vowels consisting both lower and upper letter
+vowels = ["A", "I", "U", "E", "O", "a", "i", "u", "e", "o"] 
 
-def recursive_vowel(string):    # function is defined with recursive_vowel as its name and "string" as the parameter
+def recursive_vowel(string):    
 
-    if string == "":            # The condition check if string input is an empty string or not
-        return string           # If it is an empty list, the string will then be returned
+    if string == "":            
+        return string           
     
     elif string[0] in vowels:   # The conditions check if the first letter is a vowel or not
-        return recursive_vowel(string[1:])
+        return recursive_vowel(string[1:])  # first letter skipped, and do the same for other letters. 
     
-    return string[0] + recursive_vowel(string[1:])
+    return string[0] + recursive_vowel(string[1:]) # first letter returned and do the same to other elements
 
 
 
 
 print (recursive_vowel("Adhitama Budi"))
+
+
+"""
+PSEUDOCODE:
+
+RECURSIVE_VOWEL(STRING)
+
+    if STRING = empty string
+        RETURN STRING
+
+    elif STRING[0] in VOWELS				      //base case
+
+        RETURN RECURSIVE_VOWEL(STRING[1:])
+		
+// first letter skipped and do the same to other string
+
+    return STRING[0] + 
+
+// first letter is skipped and do the same to other string
+
+"""
