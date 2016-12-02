8b41dfb6abb78b5e0609e242c59bf82cb87317be
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-12-02 19:53:50 +0000

    Update question_one.py

--- 210CT-Week-4/question_one.py
+++ 210CT-Week-4/question_one.py
@@ -1,3 +1,12 @@
+""" This is the solution for coursework question 9. It ask us to create a Binary Search Algorithm
+    and instead of outputing a value is found. We should set a range interval to check if there is 
+    a value between the range we set.
+    
+    [1, 2, 3, 4, 5]
+    Input = low = 2 High = 4
+    Output = True
+"""
+
 def binarySearch(alist, low, high):         
     first = 0                                    
     last = len(alist)-1                           
@@ -23,3 +32,38 @@ userInput_low = int(input("Please enter a low: "))
 userInput_high = int(input("Please enter a high: "))
 
 print(binarySearch(list_One, userInput_low, userInput_high))
+
+
+"""
+PSEUDOCODE:
+
+BINARYSEARCH (LISTN, LOW, HIGH)
+
+    first <- 0                                                          // (1)
+    last <- length(LISTN) - 1                                           // (1)
+    found <- FALSE                                                      // (1)
+
+    WHILE first <= last AND NOT found                                   // (n)
+        count the midpoint index                                        // (n)
+        
+        if midpoint element > LOW AND midpoint element < HIGH           // (n)
+            found <- TRUE                                               // (n)
+        
+        else                                                            // (n)
+            if midpoint element > low                                   // (n)
+                set the midpoint as the maximum boundaries  //last      // (log n)
+            else                                                        // (n)
+                set midpoint as starting point  //first                 // (log n)
+    
+    return found                                                        // (n)
+
+list_One <- [2, 3, 4, 5, 6, 7, 8]
+userInput_low <- set int input for low
+userInput_high <- set int input for high
+
+print(BINARYSEARCH(LISTN, USERINPUT_LOW, USERINPUT_HIGH)
+
+Big O Notation = O (2 log n + 8n + 3)
+Big O Notation = O (log n)
+
+"""
