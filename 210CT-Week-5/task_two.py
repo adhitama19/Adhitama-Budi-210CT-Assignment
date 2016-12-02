ba3351a7b13d6a7191df6006f308ba6438e64b99
adhitama19 <azmiia@uni.coventry.ac.uk>
2016-12-02 20:52:20 +0000

    Update task_two.py

--- 210CT-Week-5/task_two.py
+++ 210CT-Week-5/task_two.py
@@ -13,7 +13,7 @@ class List(object):
         self.tail = None
 
     def insert(self,n,x):
-    #Not actually perfect: how do we prepend to an existing file
+
         if n != None:
             x.next = n.next
             n.next = x
@@ -28,17 +28,19 @@ class List(object):
         elif self.tail == n:
             self.tail = x
 
-    def remove_node(self, n):
+    def remove_node(self, n):           # deleting node function || bits added
 
         if n.prev != None:
-            n.prev.next = n.next
+            n.prev.next = n.next        # set the next node as prev node next node
+
         else:
-            self.head = n.next
+            self.head = n.next          # set the linked list head node next
 
         if n.next != None:
-            n.next.prev = n.prev
+            n.next.prev = n.prev        # set the prev node as next node prev
+
         else:
-            self.tail = n.prev
+            self.tail = n.prev          # set the linked list head to prev node
 
 
     def display(self):
@@ -46,17 +48,32 @@ class List(object):
         values = []
         n = self.head
         while n != None:
-            
+
             values.append(str(n.value))
             n = n.next
 
-        print ("List: ", ", ".join(values))
+        print ("List: " , ", ".join(values))
 
 if __name__ == '__main__':
 
     l = List()
-    l.insert(None,Node(4))
-    l.insert(l.head,Node(6))
-    l.insert(l.head,Node(8))
-    l.remove_node(Node(4))
+
+    node_4 = Node(4)            # set variable to the Node()
+    node_6 = Node(6)
+    node_8 = Node(8)
+    node_3 = Node(3)
+    node_2 = Node(2)
+
+    l.insert(None,node_4)
+    l.insert(l.head,node_6)    # inserting a node
+    l.insert(l.head,node_8)
+    l.insert(l.tail,node_3)
+    l.insert(l.tail,node_2)
+    l.display()
+
+    l.remove_node(node_4)     # remove specific node
+    l.remove_node(node_2)
+    l.display()
+
+    l.remove_node(l.tail)     # delete node in tail loc
     l.display()
